20bf0a98f7f4dab440a829a7bf10461442c17609
ijouiri <abdellahijouiri@gmail.com>
2022-06-16 13:28:31 +0100

    Add the first layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
-
+import HomePage from "./homepage.component"
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      number: 28
-    }
-  }
-
-  handleChange = () => {
-    this.setState((prevState, prevProps) => (
-      { number: prevState.number + 1 }
-    ))
-  }
-
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.number}
-          </p>
-
-          <button onClick={this.handleChange}>Change Number</button>
-        </header>
+      <div>
+        <HomePage />
       </div>
     )
   }
